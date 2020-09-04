898498160eb6ce8dbd744d0e4975e8e93c5aae43
Aaron Smith <smitham50@gmail.com>
2020-09-04 17:26:31 -0400

    removed show and new user methods and deleted unnecessary user routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :new]
       resources :sessions
 
       post "/signup", to: "users#create"
+      get "/users", to: "users#index"
       
       # get 'sessions/new'
       # post 'sessions/create'
